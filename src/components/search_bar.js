4fca7fe084f34f2475b02a214de5a40505307e45
LilianKalkman <liliankalkman@hotmail.com>
2018-02-12 15:23:46 +0100

    start from scratch - searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { term: ''}
-  }
-
-  render (){
-    return (
-    <input
-      value= {this.state.term}
-      onChange= {event => this.setState({term: event.target.value})} />
-    )
-  }
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input></input>
+    </div>
+  );
 }
-
 
 export default SearchBar;
